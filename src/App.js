43d1a69003b451b387e1e0d49bae181827492405
suzStar <114248187+suzStar@users.noreply.github.com>
2023-02-09 18:44:06 +0000

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Suzanne Sharman and is{" "}
+        <a href="https://github.com/suzStar/weather-app-react" target="_blank">
+          {" "}
+          open-source on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
