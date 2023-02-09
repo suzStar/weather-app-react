1734aeb9babea7cd99e597f6ad3701c1dd1dccb4
suzStar <114248187+suzStar@users.noreply.github.com>
2023-02-09 18:55:38 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Suzanne Sharman and is{" "}
-        <a href="https://github.com/suzStar/weather-app-react" target="_blank">
-          {" "}
-          open-source on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Suzanne Sharman and is{" "}
+          <a
+            href="https://github.com/suzStar/weather-app-react"
+            target="_blank"
+          >
+            {" "}
+            open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
